041b8caf0ed73ac37d992677dc7174d543931936
Pedro Victor <pedrovictorveras@id.uff.br>
2023-02-07 11:40:26 -0300

    .

--- FBOAT - Iniciação Cientifica/Codigo Python para requisições MavLink/main.py
+++ FBOAT - Iniciação Cientifica/Codigo Python para requisições MavLink/main.py
@@ -5,7 +5,6 @@ app = FastAPI()
 
 
 @app.get("/")
-
 async def root():
     print('Script no terminal,uvicorn main:app --reload  ,executado com sucesso ')
-    return {"message": "Hello World", 'idade': 24}
+    return {"message": "Pedro Victor", 'idade': 24}
